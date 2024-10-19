0c22f973dde0efa7197a636b635671680c7e916d
LacamJC <joaoramajo744@gmail.com>
2024-10-18 22:06:01 -0300

    Atualizando script

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // Importe as dependencias necessarias com shelljs com 'npm i shelljs readline-sync
 const shell = require('shelljs'); 
 const readline = require('readline-sync');
+const respostas = ["s", "S", "Y", "y"]
 
 function commitAndPush(){
     const msg = readline.question("Mensagem de commit: "); // Mensagem para o commit
-    shell.exec('git add *');
-    shell.exec(`git commit -am "${msg}"`);
-    shell.exec('git push');
-    console.log('Alterações realizadas com sucesso')
+    var confirm = false;
+    console.log(`Mensagem de commit: ${msg}`);
+    confirm_msg = readline.question('Confirmar commit : ');
+    confirm_msg = confirm_msg[0].toUpperCase();
+
+    for(i=0; i < respostas.length; i++){
+        if(confirm_msg == respostas[i] && i < respostas.length){
+            console.log('Tarefa permitida'); 
+            confirm = true;
+            try{
+
+                shell.exec('git add *');
+                shell.exec(`git commit -am "${msg}"`);
+                shell.exec('git push');
+                console.log('Alterações realizadas com sucesso')
+            }catch(err){
+                console.log(`Erro ao executar script: ${err}`);
+            }
+        }
+       
+    }
+
 }
 
 commitAndPush()
