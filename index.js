bccbfe9f155cf49b3ff1041b7d8406695ca1c5f1
LacamJC <joaoramajo744@gmail.com>
2024-10-18 22:13:22 -0300

    Novas atualiza��es

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ const shell = require('shelljs');
 const readline = require('readline-sync');
 const respostas = ["s", "S", "Y", "y"]
 
+function timeOut(msg){
+    setTimeout((msg)=>{console.log(msg)},2000)
+}
+
 function commitAndPush(){
     const msg = readline.question("Mensagem de commit: "); // Mensagem para o commit
     var confirm = false;
     console.log(`Mensagem de commit: ${msg}`);
+    timeOut();
     confirm_msg = readline.question('Confirmar commit : ');
     confirm_msg = confirm_msg[0].toUpperCase();
 
    
         if(respostas.includes(confirm_msg)){
-            console.log('Tarefa permitida'); 
+            timeOut("Commit permitido")
             confirm = true;
             try{
 
                 shell.exec('git add *');
+                timeOut("Arquivos adicionados");
+                
                 shell.exec(`git commit -am "${msg}"`);
+                timeOut("Commit realizado")
                 shell.exec('git push');
-                console.log('Alterações realizadas com sucesso')
+                timeOut("Arquivos enviados para repositorio remoto")
+                console.log('Alterações realizadas com sucesso !')
             }catch(err){
                 console.log(`Erro ao executar script: ${err}`);
             }
